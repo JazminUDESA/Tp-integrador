dea0d6600a996777a00d77b367060045e05b2e4b
mariapaulaherrera <mpherrera@udesa.edu.ar>
2020-10-31 17:48:31 -0300

    carrusel

--- Javascript/pruebajava.js
+++ Javascript/pruebajava.js
@@ -8,18 +8,18 @@ window.onload = function(){
             console.log(data);
             //consultar como hacer para que te aparezca un objeto del array por cada lista
             //(y no todos los objetos dentro de una lista)
-            for (let index = 0; index < data.results.length; index++) {
+            for (let index = 0; index < 10; index++) {
                 const element = data.results[index].poster_path;
                 var li = document.querySelector(".agregarImagen");
                li.innerHTML += `<img src="https://image.tmdb.org/t/p/original${element}">`
                 
             }
 
-
         })        
         .catch(function(error) {
             console.log(`El error fue: ${error}`);
-        })
+        }) 
+
 
 
 }
