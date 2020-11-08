88ecd298ea7d3c4b577bd5870fcd3375577b4b42
Jazmín Suárez <jsuarez@udesa.edu.ar>
2020-11-08 03:30:14 -0300

    Prueba mis listas, agregue mejor calificadas (pelis y series)

--- Javascript/pruebajava.js
+++ Javascript/pruebajava.js
@@ -1,6 +1,9 @@
 window.onload = function(){
 
-    fetch(`https://api.themoviedb.org/3/movie/popular?api_key=c3dcc0e9ef8f3864ee4f5ed844d151f8`) 
+    var apiKey = `c3dcc0e9ef8f3864ee4f5ed844d151f8` 
+
+    //FETCH DEL CARRUSEL (MÁS VISTAS)
+    fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}`) 
         .then(function(response) {
             return response.json();
         })
@@ -20,11 +23,64 @@ window.onload = function(){
                        </li>
                        `
             }
+            
 
-
-        })        
+        })
         .catch(function(error) {
             console.log(`El error fue: ${error}`);
         }) 
+
+    //FETCH DE PELÍCULAS MEJORES CALIFICADAS
+    fetch(`https://api.themoviedb.org/3/movie/top_rated?api_key=${apiKey}`)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(data) {
+            console.log(data);
+
+            for (let index = 0; index < 6; index++) {
+                const element = data.results[index].poster_path;
+                var titulo = data.results[index].title;
+                var calificacion = data.results[index].vote_average;
+                var div = document.querySelector(".califPelis");
+                div.innerHTML += `
+                <a href="">
+                        <img src="${element}" alt="peliculas mejor calificadas">
+                        <h4>${titulo}</h4>
+                        <h5>Calificación: ${calificacion}</h5>
+                    </a>
+                    `
+            }
+
+        })
+        .catch(function(error) {
+            console.log(`El error fue: ${error}`);          
+        })
+
+    //FETCH DE SERIES MEJOR CALIFICADAS
+    fetch(`https://api.themoviedb.org/3/tv/top_rated?api_key=${apiKey}`)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(data) {
+            console.log(data);
+
+            for (let index = 0; index < 6; index++) {
+                const element = data.results[index].poster_path;
+                var titulo = data.results[index].name;
+                var calificacion = data.results[index].vote_average;
+                var div = document.querySelector(".califSeries");
+                div.innerHTML += `
+                <a href="">
+                        <img src="${element}" alt="series mejor calificadas">
+                        <h4>${titulo}</h4>
+                        <h5>Calificación: ${calificacion}</h5>
+                    </a>
+                    `
+            }       
+        })
+        .catch(function(error) {
+            console.log(`El error fue: ${error}`);          
+        })
 
 }
