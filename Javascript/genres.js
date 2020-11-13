9eb953552886b10e0589b021ed7550f55bffd880
mariapaulaherrera <mpherrera@udesa.edu.ar>
2020-11-13 11:58:01 -0300

    carrusel

--- Javascript/genres.js
+++ Javascript/genres.js
@@ -2,7 +2,7 @@ window.onload = function () {
 
     var apiKey = "c3dcc0e9ef8f3864ee4f5ed844d151f8"
     
-    //FETCH DEL CARRUSEL (MÁS VISTAS)
+    //FETCH DEL CARRUSEL (MÁS POPULARES)
     fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}`)
         .then(function (response) {
             return response.json();
@@ -13,7 +13,7 @@ window.onload = function () {
             for (let index = 0; index < 10; index++) {
                 const element = data.results[index].backdrop_path;
                 var titulo = data.results[index].title;
-                var ul = document.querySelector("#holi");
+                var ul = document.querySelector("#peliculas-populares");
                 ul.innerHTML += `
                        <li class="agregarImagen">
                            <img src="https://image.tmdb.org/t/p/original${element}">
