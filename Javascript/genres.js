5befb46f732b5767bdb74c347d9b7819f5e84c52
mariapaulaherrera <mpherrera@udesa.edu.ar>
2020-11-14 17:24:34 -0300

    generos

--- Javascript/genres.js
+++ Javascript/genres.js
@@ -32,8 +32,262 @@ window.onload = function () {
         })
 
     
+    //https://api.themoviedb.org/3/discover/movie?api_key=c3dcc0e9ef8f3864ee4f5ed844d151f8&with_genres=28
+
+    //FETCH PARA PELIS ACCION
+    fetch(`https://api.themoviedb.org/3/discover/movie?api_key=c3dcc0e9ef8f3864ee4f5ed844d151f8&with_genres=28&language=en`)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+
+            for (let index = 0; index < 15; index++) {
+                const element = data.results[index].poster_path;
+                
+                var ul = document.querySelector("#pelisAccion");
+                ul.innerHTML += `
+                    <li>
+                        <img src="https://image.tmdb.org/t/p/original${element}">
+                    </li>
+                `
+            }
 
 
+        })
+        .catch(function (error) {
+            console.log(`El error fue: ${error}`);
+        })
+
+    //FETCH PARA PELIS AVENTURA
+    fetch(`https://api.themoviedb.org/3/discover/movie?api_key=c3dcc0e9ef8f3864ee4f5ed844d151f8&with_genres=12&language=en`)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+
+            for (let index = 0; index < 15; index++) {
+                const element = data.results[index].poster_path;
+
+                var ul = document.querySelector("#pelisAventura");
+                ul.innerHTML += `
+                    <li>
+                        <img src="https://image.tmdb.org/t/p/original${element}">
+                    </li>
+                `
+            }
+
+
+        })
+        .catch(function (error) {
+            console.log(`El error fue: ${error}`);
+        })
+
+    //FETCH PARA PELIS CIENCIA FICCION
+    fetch(`https://api.themoviedb.org/3/discover/movie?api_key=c3dcc0e9ef8f3864ee4f5ed844d151f8&with_genres=878&language=en`)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+
+            for (let index = 0; index < 15; index++) {
+                const element = data.results[index].poster_path;
+
+                var ul = document.querySelector("#pelisCs");
+                ul.innerHTML += `
+                    <li>
+                        <img src="https://image.tmdb.org/t/p/original${element}">
+                    </li>
+                `
+            }
+
+
+        })
+        .catch(function (error) {
+            console.log(`El error fue: ${error}`);
+        })
+
+    //FETCH PARA PELIS COMEDIA
+    fetch(`https://api.themoviedb.org/3/discover/movie?api_key=c3dcc0e9ef8f3864ee4f5ed844d151f8&with_genres=35&language=en`)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+
+            for (let index = 0; index < 15; index++) {
+                const element = data.results[index].poster_path;
+
+                var ul = document.querySelector("#pelisComedia");
+                ul.innerHTML += `
+                    <li>
+                        <img src="https://image.tmdb.org/t/p/original${element}">
+                    </li>
+                `
+            }
+
+
+        })
+        .catch(function (error) {
+            console.log(`El error fue: ${error}`);
+        })
+
+    //FETCH PARA PELIS COMEDIA
+    fetch(`https://api.themoviedb.org/3/discover/movie?api_key=c3dcc0e9ef8f3864ee4f5ed844d151f8&with_genres=99&language=en`)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+
+            for (let index = 0; index < 15; index++) {
+                const element = data.results[index].poster_path;
+
+                var ul = document.querySelector("#pelisDocu");
+                ul.innerHTML += `
+                    <li>
+                        <img src="https://image.tmdb.org/t/p/original${element}">
+                    </li>
+                `
+            }
+
+
+        })
+        .catch(function (error) {
+            console.log(`El error fue: ${error}`);
+        })
+
+    
+    //FETCH PARA PELIS DRAMA
+    fetch(`https://api.themoviedb.org/3/discover/movie?api_key=c3dcc0e9ef8f3864ee4f5ed844d151f8&with_genres=18&language=en`)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+
+            for (let index = 0; index < 15; index++) {
+                const element = data.results[index].poster_path;
+
+                var ul = document.querySelector("#pelisDrama");
+                ul.innerHTML += `
+                    <li>
+                        <img src="https://image.tmdb.org/t/p/original${element}">
+                    </li>
+                `
+            }
+
+
+        })
+        .catch(function (error) {
+            console.log(`El error fue: ${error}`);
+        })
+
+    //FETCH PARA PELIS FAMILIARES
+    fetch(`https://api.themoviedb.org/3/discover/movie?api_key=c3dcc0e9ef8f3864ee4f5ed844d151f8&with_genres=10751&language=en`)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+
+            for (let index = 0; index < 15; index++) {
+                const element = data.results[index].poster_path;
+
+                var ul = document.querySelector("#pelisFam");
+                ul.innerHTML += `
+                    <li>
+                        <img src="https://image.tmdb.org/t/p/original${element}">
+                    </li>
+                `
+            }
+
+
+        })
+        .catch(function (error) {
+            console.log(`El error fue: ${error}`);
+        })
+
+    //FETCH PARA PELIS ROMANCE
+    fetch(`https://api.themoviedb.org/3/discover/movie?api_key=c3dcc0e9ef8f3864ee4f5ed844d151f8&with_genres=10749&language=en`)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+
+            for (let index = 0; index < 15; index++) {
+                const element = data.results[index].poster_path;
+
+                var ul = document.querySelector("#pelisRomance");
+                ul.innerHTML += `
+                    <li>
+                        <img src="https://image.tmdb.org/t/p/original${element}">
+                    </li>
+                `
+            }
+
+
+        })
+        .catch(function (error) {
+            console.log(`El error fue: ${error}`);
+        })
+
+    //FETCH PARA PELIS TERROR
+    fetch(`https://api.themoviedb.org/3/discover/movie?api_key=c3dcc0e9ef8f3864ee4f5ed844d151f8&with_genres=27&language=en`)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+
+            for (let index = 0; index < 15; index++) {
+                const element = data.results[index].poster_path;
+
+                var ul = document.querySelector("#pelisTerror");
+                ul.innerHTML += `
+                    <li>
+                        <img src="https://image.tmdb.org/t/p/original${element}">
+                    </li>
+                `
+            }
+
+
+        })
+        .catch(function (error) {
+            console.log(`El error fue: ${error}`);
+        })
+    
+    //FETCH PARA PELIS THRILLERS
+    fetch(`https://api.themoviedb.org/3/discover/movie?api_key=c3dcc0e9ef8f3864ee4f5ed844d151f8&with_genres=53&language=en`)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+
+            for (let index = 0; index < 15; index++) {
+                const element = data.results[index].poster_path;
+
+                var ul = document.querySelector("#pelisThrillers");
+                ul.innerHTML += `
+                    <li>
+                        <img src="https://image.tmdb.org/t/p/original${element}">
+                    </li>
+                `
+            }
+
+
+        })
+        .catch(function (error) {
+            console.log(`El error fue: ${error}`);
+        })
+
+
+
+    
 
     //FETCH DEL CARRUSEL (MÁS POPULARES - SERIES)
     fetch(`https://api.themoviedb.org/3/tv/popular?api_key=${apiKey}`)
