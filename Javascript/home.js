5f509fd7745f7cd340c45408198511bb8fd03843
catalina <csonciniviola@udesa.edu.ar>
2020-11-15 04:37:11 -0300

    header desktop + banner más vistas (mobile y desktop)

--- Javascript/home.js
+++ Javascript/home.js
@@ -1,6 +1,6 @@
 window.onload = function(){
 
-    var apiKey = `c3dcc0e9ef8f3864ee4f5ed844d151f8` 
+    var apiKey = `c3dcc0e9ef8f3864ee4f5ed844d151f8`;
 
     //FETCH DEL CARRUSEL (MÁS VISTAS)
     fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}`) 
@@ -10,18 +10,19 @@ window.onload = function(){
         .then(function(data) {
             console.log(data);
 
-            for (let index = 0; index < 10; index++) {
+            for (let index = 0; index < data.results.length; index++) {
                 const element = data.results[index].backdrop_path;
                 var titulo = data.results[index].title;
-                   var ul = document.querySelector("#holi");
-                   ul.innerHTML += `
-                       <li class="agregarImagen">
-                           <img src="https://image.tmdb.org/t/p/original${element}">
-                           <div class="uk-overlay uk-overlay-primary uk-position-bottom uk-text-center uk-transition-slide-bottom">
-                                <h3 id="fondobanner" class="uk-margin-remove">${titulo}</h3>
-                            </div>
-                       </li>
-                       `
+                var ul = document.querySelector("#pelisPopularesBanner");
+                ul.innerHTML += 
+                `<li class="bannerImg">
+                    <img src="https://image.tmdb.org/t/p/original${element}">
+                    <div id="titulosBanner" class="uk-position-center-left uk-position-small uk-text-center uk-light">
+                        <h2 id="titulo-banner">Más vistas</h3>
+                        <h2 id="titulo-pelicula" class="uk-margin-remove">${titulo}</h2>
+                    </div>
+                </li>
+                `
             }
             
 
@@ -29,6 +30,8 @@ window.onload = function(){
         .catch(function(error) {
             console.log(`El error fue: ${error}`);
         }) 
+
+
 
     //FETCH DE PELÍCULAS MEJORES CALIFICADAS
     fetch(`https://api.themoviedb.org/3/movie/top_rated?api_key=${apiKey}`)
