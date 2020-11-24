c91cb23f6554bf39b14248adc6408b51b912fe80
catalina <csonciniviola@udesa.edu.ar>
2020-11-24 14:21:18 -0300

    función agregar/sacar favs y que se mantenga

--- Javascript/favoritos.js
+++ Javascript/favoritos.js
@@ -1,6 +1,7 @@
 window.onload = function () {
     var apiKey = `c3dcc0e9ef8f3864ee4f5ed844d151f8`
-    // ID PELI FAV
+    
+    // Películas favoritas
     var idPelisFavoritas = JSON.parse(localStorage.getItem("idPelisFavs"));
     console.log(idPelisFavoritas);
 
@@ -18,16 +19,40 @@ window.onload = function () {
 
                 var divPelisFavoritas = document.querySelector("#divPelisFavoritas");
                 divPelisFavoritas.innerHTML += 
-                `<a class="agregarPosterPeliFav" href="detalles.html?tipo=peliculas&id=${id}"><img src="https://image.tmdb.org/t/p/original${data.poster_path}"></a>`
-
+                `<a class="agregarPosterPeliFav" href="detalles.html?tipo=peliculas&id=${id}">
+                <img src="https://image.tmdb.org/t/p/original${data.poster_path}">
+                </a>`;
             })
             .catch(function(error) {
                 console.log(`El error fue: ${error}`);          
-            })
+            })    
+    }
+    
+    // Series favoritas
+    var idSeriesFavoritas = JSON.parse(localStorage.getItem("idSeriesFavs"));
+    console.log(idSeriesFavoritas);
 
-            
-            
-        
+    for (let i = 0; i < idSeriesFavoritas.length; i++) {
+        const id = idSeriesFavoritas[i];
+
+
+        fetch(`https://api.themoviedb.org/3/tv/${id}?api_key=${apiKey}`) 
+
+            .then(function(response) {
+                return response.json();
+            })
+            .then(function(data) {
+                console.log(data);
+
+                var divSeriesFavoritas = document.querySelector("#divSeriesFavoritas");
+                divSeriesFavoritas.innerHTML += 
+                `<a class="agregarPosterSerieFav" href="detalles.html?tipo=peliculas&id=${id}">
+                    <img src="https://image.tmdb.org/t/p/original${data.poster_path}">
+                </a>`;
+            })
+            .catch(function(error) {
+                console.log(`El error fue: ${error}`);          
+            })    
     }
     
     
